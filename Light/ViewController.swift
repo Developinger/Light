c6c437e298d7ee5b4dc80322b1913fc6aef16655
Homaine Blair <homaineblair@gmail.com>
2022-09-16 14:01:38 -0400

    v1

--- Light/ViewController.swift
+++ Light/ViewController.swift
@@ -9,11 +9,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var lightButton: UIButton!
+    
+    var lightOn = true
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+}
+
+    fileprivate func extractedFunc() {
+        if lightOn {
+            view.backgroundColor = .white
+            lightButton.setTitle("Off", for: .normal)
+            
+        }
+        else {
+            view.backgroundColor = .black
+            lightButton.setTitle("On", for: .normal)
+
+        }
     }
-
-
+    
+    @IBAction func buttonPressed(_ sender: Any) {
+        lightOn.toggle()
+        extractedFunc()
+    }
 }
 
