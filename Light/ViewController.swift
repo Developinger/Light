5e42d1ff553eb4eaec0deb6e4cbea724414e88d7
Homaine Blair <homaineblair@gmail.com>
2022-09-16 13:10:10 -0400

    Initial Commit

---
+++ Light/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Light
+//
+//  Created by 1a on 9/16/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
